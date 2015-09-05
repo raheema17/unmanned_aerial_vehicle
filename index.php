4ff0a881a26ae0a436d87a47bc6d7b5c98a7d670
Raheema <raheemasulthana@live.com>
2015-09-05 22:47:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
    <!--- Basic Page Needs
    ================================================== -->
 	<meta charset="utf-8">
-	<title>Unmanned Aerial Vehicle</title>
+	<title>Unmanned Aerial Vehicle Sample</title>
 	<meta name="description" content="">
 	<meta name="author" content="">
 
